c46034641699b7f6b0f73121d4a177de8c626261
wizz13150 <lewizard@hotmail.fr>
2019-05-22 13:03:39 +0200

    Add files via upload

---
+++ Scripts/WindowsActivation/Activate_Win_KMS.bat
@@ -0,0 +1,7 @@
+@ECHO
+c:
+cd c:\windows\system32
+cscript.exe slmgr.vbs /skms KMSServe.contonso.com
+cscript.exe slmgr.vbs /upk
+cscript.exe slmgr.vbs /ipk key-key-key-key-key
+cscript.exe slmgr.vbs /ato
