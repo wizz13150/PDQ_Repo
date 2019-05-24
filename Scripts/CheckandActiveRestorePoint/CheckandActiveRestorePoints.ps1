17bf36b67eec0f07eaedcb59d53f4a2cfb275d09
wizz13150 <lewizard@hotmail.fr>
2019-05-24 14:02:44 +0200

    Add files via upload

---
+++ Scripts/CheckandActiveRestorePoint/CheckandActiveRestorePoints.ps1
@@ -0,0 +1,12 @@
+﻿$ScriptBlock = {    
+    Get-ComputerRestorePoint -OutVariable RestorePoints
+ 
+    if(-not($RestorePoints)){
+        
+        $env:COMPUTERNAME
+ 
+        Enable-ComputerRestore -Drive C: -Verbose
+    }
+}
+ 
+Invoke-Command -ScriptBlock $ScriptBlock
