ee3f8f0c4135f31904a9117d2a887a47b2b60720
Wizz_^ <lewizard@hotmail.fr>
2019-07-15 09:45:26 +0200

    Update CheckUserProfileSize.ps1

--- Scripts/CheckADUser/CheckUserProfileSize.ps1
+++ Scripts/CheckADUser/CheckUserProfileSize.ps1
@@ -1,10 +1,12 @@
-﻿# Get the cumulative size of all user profiles on the system in GB
+﻿# Get the cumulative size of all user profiles on the system in MB
+ # Can be on local disk, OR a network share for non local profiles
+ # Run as 'Logged On User' for PDQ Deploy !
 
 $Sum = [math]::Round(
   (
     (
       (
-        Get-ChildItem -Path "\\home.adcha.local\USERS$\$env:username" |
+        Get-ChildItem -Path "\\FileServer\USERS$\$env:username" |
         ForEach-Object {
           Write-Host "Analyse du fichier $($_.FullName)" -ForegroundColor Green ;
           Get-ChildItem -Path $_.FullName -Recurse -File -ErrorAction SilentlyContinue } | Measure-Object -Property Length -Sum).Sum)/1MB 
@@ -13,4 +15,4 @@ $Sum = [math]::Round(
 write-Host "Taille totale des données contenus dans le dossier utilisateur de $env:username :" -ForegroundColor Cyan
 Write-Host "$Sum Mb" -ForegroundColor Cyan
 Write-Host "Chemin analysé :" -ForegroundColor Cyan
-Write-Host "\\home.adcha.local\USERS$\$env:username" -ForegroundColor Cyan
+Write-Host "\\FileServer\USERS$\$env:username" -ForegroundColor Cyan
