69c5bc15c2c68eded203d6b53d38a930e3294cdf
Wizz_^ <lewizard@hotmail.fr>
2019-07-15 09:43:59 +0200

    Add files via upload

---
+++ Scripts/CheckADUser/CheckUserProfileSize.ps1
@@ -0,0 +1,16 @@
+﻿# Get the cumulative size of all user profiles on the system in GB
+
+$Sum = [math]::Round(
+  (
+    (
+      (
+        Get-ChildItem -Path "\\home.adcha.local\USERS$\$env:username" |
+        ForEach-Object {
+          Write-Host "Analyse du fichier $($_.FullName)" -ForegroundColor Green ;
+          Get-ChildItem -Path $_.FullName -Recurse -File -ErrorAction SilentlyContinue } | Measure-Object -Property Length -Sum).Sum)/1MB 
+  )
+)
+write-Host "Taille totale des données contenus dans le dossier utilisateur de $env:username :" -ForegroundColor Cyan
+Write-Host "$Sum Mb" -ForegroundColor Cyan
+Write-Host "Chemin analysé :" -ForegroundColor Cyan
+Write-Host "\\home.adcha.local\USERS$\$env:username" -ForegroundColor Cyan
